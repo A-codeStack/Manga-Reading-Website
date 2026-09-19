db771d2f794354cef54c033bbd09c8c1560238c8
A-codeStack <dreibanaag13@gmail.com>
2026-09-19 20:05:58 +0800

    Full DOM for script

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,92 @@
 const ul = document.querySelector('.list')
-const new_item = document.querySelector('.added_item')
-const add_button = document.querySelector('.add_button')
-const list_items = document.querySelectorAll('.list_item')
-const items_array = Array.from(list_items)
-const delete_button = document.querySelector('.delete_button')
+const add = document.querySelector('.add-button')
+const input = document.querySelector('.add-input')
+const search = document.querySelector('.search')
+const list_items = document.querySelectorAll('li')
+const delete_button = document.querySelector('.delete-button')
 
-add_button.addEventListener('click', () => {
+add.addEventListener('click', (e) => {
+  if(input.value == ""){
+    console.log("Input value Empty: Please Enter title")
+  }else{
+    //Create Element
+    const li = document.createElement('li')
+    const delete_button = document.createElement('button')
+    const p = document.createElement('p')
+    const edit_button = document.createElement('button')
+    const submit_edit = document.createElement('button')
+    const edit_input = document.createElement('input')
 
-  const new_title = new_item.value.trim()
+    delete_button.classList.add('delete-button')
+    submit_edit.classList.add('submit-edit')
+    li.classList.add('list-item')
+    edit_button.classList.add('edit-button')
+    edit_input.classList.add('edit-input')
+    edit_input.placeholder = "Enter New Title";
+    edit_input.type = "text"
 
-  if(new_title !== ""){
-    let new_li = document.createElement('li')
 
-    let new_p = document.createElement('p')
-    let new_delete = document.createElement('button')
+    delete_button.textContent = 'Delete'
+    edit_button.textContent = 'Edit'
+    submit_edit.textContent = "Submit"
+    p.textContent = input.value  
 
-    new_delete.textContent = 'delete'
-    new_delete.classList.add('delete_button')
-    console.log(new_delete)
+    li.append(p)
+    li.append(delete_button)
+    li.append(edit_button)
+    li.append(edit_input)
+    li.append(submit_edit)
+    ul.prepend(li)
 
-    new_li.classList.add('list_item')
-    new_p.classList.add('item_title')
-    new_p.textContent = new_title
 
-    new_li.append(new_p)
-    new_li.append(new_delete)
-    ul.prepend(new_li)
-  }else{                           
-    console.log("Please fill up: Empty Title")
+    input.value = ""
   }
+})
+
+
+search.addEventListener('input', (e) => {
+  
+  list_items.forEach(item => {
+
+    if(item.firstElementChild.textContent.trim().toLowerCase().includes(e.target.value)){
+      item.style.display = "block"
+    }else{
+      item.style.display = "none"
+    }
+  })
+  
 })
 
 
 ul.addEventListener('click', (e) => {
-  if(e.target == delete_button){
-    delete_button.parentElement.remove()
+  if(e.target.className == 'delete-button'){
+    console.log("target hit")
+    const Del_button = e.target
+    Del_button.closest('.list-item').remove()
+  } 
+  else if (e.target.className == 'edit-button'){
+    const edit_field = e.target.nextElementSibling;
+    edit_field.style.display="block"
+
+    const edit_submit = edit_field.nextElementSibling
+    edit_submit.style.display="block"
+
+    edit_submit.addEventListener('click', (e) => {
+      if (e.target.previousElementSibling.value == ""){
+        console.log("Error: Please Enter new title")
+      }else{
+        const new_title_field = e.target.previousElementSibling
+        const new_title = new_title_field.value
+        console.log("new Title:", new_title)
+        const title_element = e.target.closest('.list-item').firstElementChild
+        title_element.textContent = new_title
+
+        new_title_field.value = ""
+        edit_field.style.display = "none"
+        edit_submit.style.display = "none"
+      }
+    })
+
   }
 })
 
-
-const search_input = document.querySelector('.search')
