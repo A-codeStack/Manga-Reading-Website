2fe937c469ec65ac1f54aa8c303e9cf0d6795adc
A-codeStack <dreibanaag13@gmail.com>
2026-09-26 01:07:14 +0800

    todo app custom modal

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,10 @@ const input = document.querySelector('.add-input')
 const search = document.querySelector('.search')
 const list_items = document.querySelectorAll('li')
 const delete_button = document.querySelector('.delete-button')
+const modal = document.querySelector('.modal')
+const modal_title = document.querySelector('.modal-title')
+const modal_body = document.querySelector('.modal-body')
+
 
 add.addEventListener('click', (e) => {
   if(input.value == ""){
@@ -16,7 +20,11 @@ add.addEventListener('click', (e) => {
     const edit_button = document.createElement('button')
     const submit_edit = document.createElement('button')
     const edit_input = document.createElement('input')
+    const item_div = document.createElement('div')
+    const button_div = document.createElement('div')
 
+    item_div.classList.add('list-title')
+    button_div.classList.add('list-buttons')
     delete_button.classList.add('delete-button')
     submit_edit.classList.add('submit-edit')
     li.classList.add('list-item')
@@ -30,18 +38,19 @@ add.addEventListener('click', (e) => {
     edit_button.textContent = 'Edit'
     submit_edit.textContent = "Submit"
     p.textContent = input.value  
-
-    li.append(p)
-    li.append(delete_button)
-    li.append(edit_button)
-    li.append(edit_input)
-    li.append(submit_edit)
+  
+    item_div.append(p)
+    button_div.append(delete_button)
+    button_div.append(edit_button)
+    button_div.append(edit_input)
+    button_div.append(submit_edit)
+    li.append(item_div)
+    li.append(button_div)
     ul.prepend(li)
 
     input.value = ""
   }
 })
-
 
 search.addEventListener('input', (e) => {
   
@@ -56,34 +65,46 @@ search.addEventListener('input', (e) => {
   
 })
 
-
 ul.addEventListener('click', (e) => {
+
+
+
   if(e.target.className == 'delete-button'){
     const Del_button = e.target
     Del_button.closest('.list-item').remove()
-  } 
-  else if (e.target.className == 'edit-button'){
-    const edit_field = e.target.nextElementSibling;
-    edit_field.style.display="block"
-
-    const edit_submit = edit_field.nextElementSibling
-    edit_submit.style.display="block"
-
-    edit_submit.addEventListener('click', (e) => {
-      if (e.target.previousElementSibling.value == ""){
-        console.log("Error: Please Enter new title")
-      }else{
-        const new_title_field = e.target.previousElementSibling
-        const new_title = new_title_field.value
-        console.log("new Title:", new_title)
-        const title_element = e.target.closest('.list-item').firstElementChild
-        title_element.textContent = new_title
-
-        new_title_field.value = ""
-        edit_field.style.display = "none"
-        edit_submit.style.display = "none"
-      }
-    })
+  } if (e.target.className == 'edit-button'){
+    modal.style.display ="flex"
+    const li = e.target.closest('.list-item')
+    const item_title = li.firstElementChild.firstElementChild.textContent
+    const input_old_title = modal_body.firstElementChild
+    input_old_title.value = item_title
+    modalFunction(input_old_title)
   }
 })
 
+const modalFunction = (old_title) =>{
+  modal.addEventListener('click', (e) => {
+    if (e.target.className.includes('cancel-edit')) {
+      closeModal()
+    } else if (e.target.className.includes('submit-edit')) {
+      console.log("Clicked Submit")
+      old_title = old_title.value
+      old_title = old_title.trim().toLowerCase()
+      const new_title = modal_body.firstElementChild.value
+      if(old_title === new_title.trim().toLowerCase()){
+        alert("No changes detected: submit unsuccesful")
+      }else{
+        modal_body.firstElementChild.value = new_title
+        console.log(modal_body.firstElementChild.value)
+      }
+    }
+  })
+}
+
+let closeModal = () => {
+  modal.style.display = 'none'
+}
+
+let openModal = () =>{
+  modal.style.display = ''
+}
